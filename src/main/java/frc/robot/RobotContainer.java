6d8db04c7a7983d32b11d9a3433496cb6f7b1e62
Podcakero <35536694+Podcakero@users.noreply.github.com>
2025-04-21 17:00:35 +0000

    Apply spotless formatting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+        // Copyright 2002-2025 FRC 1629        // http://github.com/GaCo-1629        //        // This program is free software; you can redistribute it and/or        // modify it under the terms of the GNU General Public License        // version 3 as published by the Free Software Foundation or        // available in the root directory of this project.        //        // This program is distributed in the hope that it will be useful,        // but WITHOUT ANY WARRANTY; without even the implied warranty of        // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        // GNU General Public License for more details.        
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
